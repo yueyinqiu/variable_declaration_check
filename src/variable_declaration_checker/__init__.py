0472197d8231c24852b8bb1a055c2420c7d2cf6d
yueyinqiu <yueyinqiu@outlook.com>
2024-10-15 19:09:59 +0800

    c

--- src/variable_declaration_checker/__init__.py
+++ src/variable_declaration_checker/__init__.py
@@ -18,7 +18,7 @@ class VariableDeclarationChecker(pylint.checkers.BaseChecker):
     def check_locals(self, locals_dictionary: dict[str, list[astroid.nodes.NodeNG]]):
         for name, occurrences in locals_dictionary.items():
             if name == "_":
-                break
+                continue
 
             occurrence_iterator = iter(occurrences)
             occurrence = next(occurrence_iterator)
